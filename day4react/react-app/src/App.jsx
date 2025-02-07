d73d0b0b091eb39d5d64fdd55978a184cb5b3519
Prabhat-kumar-11 <prabhssgg3@gmail.com>
2025-02-07 12:34:49 +0530

    component

---
+++ day4react/react-app/src/App.jsx
@@ -0,0 +1,12 @@
+const App = () => {
+  return (
+    <div>
+      <h1 className="heading" style={{ color: "red", fontSize: "45px" }}>
+        hello world
+      </h1>
+      <h2>hello</h2>
+    </div>
+  );
+};
+
+export default App;
