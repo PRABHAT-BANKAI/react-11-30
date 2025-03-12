fabe7ca8f3bd2dba6796431b5774def7bbfa29aa
Prabhat-kumar-11 <prabhssgg3@gmail.com>
2025-03-12 12:29:58 +0530

    filterandSortING

--- day19json/crud/src/components/Home.jsx
+++ day19json/crud/src/components/Home.jsx
@@ -5,6 +5,32 @@ import { Link } from "react-router-dom";
 const Home = () => {
   const [products, setProducts] = useState([]);
 
+  // const [category, setCategory] = useState("");
+
+  function handleFilter(value) {
+    let productData = products;
+    console.log(value);
+    if (value === "all") {
+      axios.get("http://localhost:3000/product").then((res) => {
+        setProducts(res.data);
+      });
+      return;
+    }
+    let filterData = productData.filter((prod) => prod.description === value);
+    setProducts(filterData);
+  }
+
+  function handleSort() {
+    let newSort = products.sort((a, b) => a.price - b.price);
+    console.log(newSort);
+    setProducts([...newSort]);
+  }
+  function handleSort2() {
+    let newSort = products.sort((a, b) => b.price - a.price);
+    console.log(newSort);
+    setProducts([...newSort]);
+  }
+
   function handleDelete(id) {
     axios
       .delete("http://localhost:3000/product/" + id)
@@ -31,9 +57,17 @@ const Home = () => {
   return (
     <div>
       <Link to={"/createProduct"}>
-        {" "}
         <button>Create Product </button>
       </Link>
+      <button onClick={handleSort}>low to high</button>
+      <button onClick={handleSort2}>high to low</button>
+      <select name="" id="" onChange={(e) => handleFilter(e.target.value)}>
+        <option defaultValue={""}>Select Category</option>
+        <option value="all">All</option>
+        <option value="slipper">Slipper</option>
+        <option value="shoes">Shoes</option>
+        <option value="sandals">Sandals</option>
+      </select>
       <table border={""}>
         <thead>
           <tr>
