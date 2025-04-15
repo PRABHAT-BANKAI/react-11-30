56fa36e71c0ec826945956a770f1cca7b3acf232
Prabhat-kumar-11 <prabhssgg3@gmail.com>
2025-04-15 12:47:02 +0530

    crudwithfirbase

--- day28Storage/storage/src/App.jsx
+++ day28Storage/storage/src/App.jsx
@@ -1,10 +1,19 @@
-import { addDoc, collection, getDocs } from "firebase/firestore";
+import {
+  addDoc,
+  collection,
+  deleteDoc,
+  doc,
+  getDocs,
+  updateDoc,
+} from "firebase/firestore";
 import { useEffect, useState } from "react";
 import { db } from "./config/firebase";
 
 const App = () => {
   const [inputText, setInputText] = useState("");
   const [todolist, setTodolist] = useState([]);
+  const [boolean, setBoolean] = useState(false);
+  const [editID, setEditID] = useState(null);
 
   const database = collection(db, "todolists");
 
@@ -17,6 +26,7 @@ const App = () => {
     try {
       await addDoc(database, { todo: inputText });
       alert("Task added successfully");
+      getData();
     } catch (error) {
       console.log(error);
       alert("Error adding task");
@@ -34,7 +44,29 @@ const App = () => {
       }))
     );
   }
-  console.log(todolist);
+  const handleDelete = async (id) => {
+    let DeleteData = doc(database, id);
+    await deleteDoc(DeleteData);
+    alert("Task deleted successfully");
+    getData();
+  };
+  const handleEdit = async (data) => {
+    setInputText(data.todo)
+    setBoolean(true);
+    setEditID(data.id);
+  }
+  const handleUpdate = async ()=>{
+    if(!inputText.trim()){
+      alert("Please enter a task");
+      return;
+    }
+    let updateValue = doc(database, editID);
+    await updateDoc(updateValue,{todo:inputText})
+    alert("Task updated successfully");
+    setInputText("");
+    setBoolean(false);
+    getData();
+  }
 
   useEffect(() => {
     getData();
@@ -48,15 +80,37 @@ const App = () => {
         value={inputText}
         onChange={(e) => setInputText(e.target.value)}
       />
-      <button onClick={handleAdd}>Add</button>
+      {boolean ? (
+        <button onClick={handleUpdate}>Update</button>
+      ) : (
+        <button onClick={handleAdd}>Add</button>
+      )}
       <div>
-        {todolist.length ==0? <h1>add you task</h1>:todolist.map((todo)=>{
-        return (
-          <div key={todo.id}>
-            <h1>{todo.todo}</h1>
-          </div>
-        );
-        })}
+        {todolist.length == 0 ? (
+          <h1>add you task</h1>
+        ) : (
+          todolist.map((todo) => {
+            return (
+              <div key={todo.id}>
+                <h1>{todo.todo}</h1>
+                <button
+                  onClick={() => {
+                    handleDelete(todo.id);
+                  }}
+                >
+                  Delete
+                </button>
+                <button
+                  onClick={() => {
+                    handleEdit(todo);
+                  }}
+                >
+                  Edit
+                </button>
+              </div>
+            );
+          })
+        )}
       </div>
     </div>
   );
