e67ac572e5c696315fc5498d83c1e4d94a8e8f54
Prabhat-kumar-11 <prabhssgg3@gmail.com>
2025-04-16 12:22:49 +0530

    updatewithvercel

--- day18routing/routing/src/components/SinglePage.jsx
+++ day18routing/routing/src/components/SinglePage.jsx
@@ -1,12 +1,15 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 const SinglePage = () => {
   const [singleData, setSingleData] = useState();
   const { id } = useParams();
+  const navigate = useNavigate();
   console.log(id);
-
+  function handleBack() {
+    navigate("/contact");
+  }
   useEffect(() => {
     axios.get("https://fakestoreapi.com/products/" + id).then((res) => {
       setSingleData(res.data);
@@ -20,8 +23,11 @@ const SinglePage = () => {
           <p>Title: {singleData.title}</p>
           <img src={singleData.image} alt="" className="h-60" />
           <p>Price: {singleData.price}</p>
-          <p>Description: {singleData.description.slice(0,24)}</p>
-          <button>Buy Now</button>
+          <p>Description: {singleData.description.slice(0, 24)}</p>
+          <button>Buy Now</button>{" "}
+          <button className="text-[blue]" onClick={handleBack}>
+            Back{" "}
+          </button>
         </div>
       )}
     </>
